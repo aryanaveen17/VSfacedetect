fb1dbddeafa69c76a461cc4c3fe0e035cf1a7d7d
aryanaveen17 <54345072+aryanaveen17@users.noreply.github.com>
2019-08-21 13:18:19 +0530

    Add files via upload
    Has all functions for the videostream on Matlab
    Function call change might be required based on file name as uploaded first stable copy.

---
+++ myPrediction2.m
@@ -0,0 +1,28 @@
+function detected = myPrediction(testImage,sceneFeatures,nFaces)
+fcnHandle = @(x) detectFASTFeatures(x,...
+	'MinQuality',0.025,...
+	'MinContrast',0.025); %#ok
+extractorMethod = 'SURF'; %#ok
+metric = 'SAD'; %#ok
+adjustHistograms = false;
+if adjustHistograms
+	testImage = histeq(testImage);
+end
+
+boxPoints = fcnHandle(testImage);
+[boxFeatures, boxPoints] = extractFeatures(testImage, boxPoints,...
+	'Method',extractorMethod,...
+	'BlockSize',3,...
+	'SURFSize',64);
+matchMetric = zeros(size(boxFeatures,1),nFaces);
+for ii = 1:nFaces
+	[~,matchMetric(:,ii)] = matchFeatures(boxFeatures,sceneFeatures{ii},...
+		'MaxRatio',1,...
+		'MatchThreshold',100,...
+		'Metric',metric);
+end
+if min(mean(matchMetric)) > 1.5
+	detected = 0;
+else
+	[~,detected] = min(mean(matchMetric));
+end
